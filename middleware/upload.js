891ea1213cf7c2d0e7afa344773a7af0bc9cb8c3
Vitalii <vitalii.kostiura@gmail.com>
2022-08-23 22:34:59 +0300

    created configuration for  uploads files

---
+++ middleware/upload.js
@@ -0,0 +1,28 @@
+const multer = require('multer')
+const moment = require('moment')
+
+const storage = multer.diskStorage({
+    destination(req, file, callback) {
+        callback(null, 'uploads/')
+    },
+    filename(req, file, callback) {
+        const date = moment().format('DDMMYYYY-HHmmss_SSS')
+callback(null, `${date}-${file.originalname}`)
+    }
+})
+const fileFilter = (req, file, callback) => {
+if(file.mimetype === 'img/png' || file.mimetype === 'image/jpeg') {
+callback(null, true)
+} else {
+    callback(null, false)
+}
+}
+
+const limits = {
+    fileSize: 1024 * 1024 * 5
+}
+module.exports = multer({ //configuration object
+storage: storage,
+fileFilter: fileFilter,
+limits: limits
+})
